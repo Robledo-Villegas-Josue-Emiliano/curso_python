2607c3a4247d479f657a662b709cf57fe0d2b3a7
Robledo-Villegas-Josue-Emiliano <RobledoTorpedo89@gmail.com>
2026-01-21 18:52:14 -0700

    Board is comming

---
+++ tictactoe/README.md
@@ -0,0 +1,7 @@
+# Tictactoe
+
+A game of tic-tac-toe
+implemented in Python
+
+# Author
+Developed by Josué Emiliano Robledo Villegas
--- tictactoe/game_logic/board.py
+++ tictactoe/game_logic/board.py
@@ -3,17 +3,22 @@ Docstring for tictactoe.
 game_logic.board
 Author : Josué Emiliano Robledo Villegas
 
-""""
-def display board(dboard:dict) -> None:
-    """ Display game board 
-    of Tictactoe
-    """"
+"""
+def display_board(dboard:dict) -> None:
+    """Display game board of Tictactoe"""
+
     d = dboard
-    print(f"{d[0]:2s}|{d[1]:2s}|{d[2]:2s}")
-    print("-----|-----|-----")
-    print(f"{d[3]:2s}|{d[4]:2s}|{d[5]:2s}")
-    print("-----|-----|-----")
-    print(f"{d[6]:2s}|{d[7]:2s}|{d[8]:2s}")
+    print(f"{d[0]:2s}|{d[1]:2}|{d[2]:2s}")
+    print("--+--+--")
+    print(f"{d[3]:2s}|{d[4]:2}|{d[5]:2s}")
+    print("--+--+--")
+    print(f"{d[6]:2s}|{d[7]:2}|{d[8]:2s}")
 
+def player_turn(player:str, dboard:dict)->int:
+    """Ask player for their turns"""
 
-
+if __name__ == "__main__":
+    board = {x:str(x) for x in range(9)}
+    board[0] = "X"
+    board[4] = "O"
+    display_board(board)
