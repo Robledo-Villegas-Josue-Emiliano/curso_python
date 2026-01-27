fe9bd6b34b4a3476ca56135de1f5c792222697fd
Robledo-Villegas-Josue-Emiliano <RobledoTorpedo89@gmail.com>
2026-01-26 18:54:42 -0700

    game avance

--- tictactoe/game_logic/board.py
+++ tictactoe/game_logic/board.py
@@ -1,24 +1,23 @@
 """
-Docstring for tictactoe.
-game_logic.board
-Author : Josué Emiliano Robledo Villegas
-
+Docstring for tictactoe.game_logic.board
+Author: Josué Emiliano Robledo Villegas
 """
-def display_board(dboard:dict) -> None:
-    """Display game board of Tictactoe"""
 
+def display_board(dboard:dict)->None:
+    """ Display game board of Tictactoe
+    """
     d = dboard
-    print(f"{d[0]:2s}|{d[1]:2}|{d[2]:2s}")
+    print(f"{d[0]:2s}|{d[1]:2s}|{d[2]:2s}")
     print("--+--+--")
-    print(f"{d[3]:2s}|{d[4]:2}|{d[5]:2s}")
+    print(f"{d[3]:2s}|{d[4]:2s}|{d[5]:2s}")
     print("--+--+--")
-    print(f"{d[6]:2s}|{d[7]:2}|{d[8]:2s}")
+    print(f"{d[6]:2s}|{d[7]:2s}|{d[8]:2s}")
 
 def player_turn(player:str, dboard:dict)->bool:
-    """Ask player for their turns"""
-
-    valid_move = False
-    user_input = input(f"Player {player} enter your move (0-8): ")
+    """ Ask player for their turn
+    """
+    valid_move = False 
+    user_input = input(f"Player {player}, enter your move (0-8): ")
     user_input = int(user_input)
     print(f"Value entered: {user_input} type: {type(user_input)}")
     if user_input in dboard.keys():
@@ -28,17 +27,20 @@ def player_turn(player:str, dboard:dict)->bool:
         else:
             print("Invalid move: Cell already occupied.")
     else:
-        print("Invalid move: Cell does not exist")
+        print("Invalid move: Cell does not exist.")
     return valid_move
 
 if __name__ == "__main__":
     board = {x:str(x) for x in range(9)}
     display_board(board)
     move = player_turn('X', board)
-    print (f"Move valid: {move}")
+    print(f"Move valid: {move}")
     display_board(board)
-
+    
     move = player_turn('O', board)
     print(f"Move valid: {move}")
     display_board(board)
     print(board)
+    #board[0] = 'X'
+    #board[4] = 'O'
+    #display_board(board)
