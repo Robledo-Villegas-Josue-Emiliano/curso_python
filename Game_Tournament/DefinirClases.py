4a491f21f88b710e85226eea3697560721d4d49a
Robledo-Villegas-Josue-Emiliano <RobledoTorpedo89@gmail.com>
2026-02-05 18:42:27 -0700

    New game, Game tournament

--- Game_Tournament/DefinirClases.py
+++ Game_Tournament/DefinirClases.py
@@ -3,16 +3,21 @@ class Athlete:
     def __init__(self,name):
         """Custom factory function of class Athlete, receives name"""
         self.name = name
+        self.number = 0
 
     def __str__(self):
         """String representation of the Athlete object."""
         return F"Athlete name is {self.name}"
     
     def __repr__(self):
-        return F"Athlete name is {self.name}, Number: {self.number}"
+        return F"Athlete (name is {self.name}, Number: {self.number})"
+    
+    def set_number(self, number):
+        """Set the athelete´s number."""
+        self.number = number
 
 if __name__=="__main__":
   Athlete1 = Athlete("Lionel Messi")
-  Athlete1.number = 10
+  Athlete1.set_number = 10
   print ( Athlete1)
   print(repr(Athlete1))
