289c4461d164375077096d3b608a8d238222d66e
Robledo-Villegas-Josue-Emiliano <RobledoTorpedo89@gmail.com>
2026-02-10 18:30:20 -0700

    add "add_name" to Sport

--- Game_Tournament/Sport.py
+++ Game_Tournament/Sport.py
@@ -1,11 +1,26 @@
 """Sport class rpresents a sport in the tournament, It has a name and a league."""
 class Sport:
     """Sport class represents a asport in the tournament. It has a name and a league"""
+    max_score = {
+         "Futbol" : 7,
+         "Baseball" : 20,
+         "Basketball" : 130,
+         "Futbol Americano" : 60,
+         "Hockey" : 10
+    }
     def __init__(self, name, num_players, league):
         """Custom constructor for sport class."""
         self.name = name
         self.league = league
         self.num_players = num_players
+
+    def add_name (self, name):
+        """"Add name to the sport"""
+        if name in self.max_score:
+            self.name = name
+        else:
+            raise ValueError(f"Sport name must ve one of the following {', '.join(self.max_score.keys())}")
+         
     def __str__(self):
         "String representation of the Sport Class. "
         return f"{self.name} ({self.league}) - {self.num_players} players"
