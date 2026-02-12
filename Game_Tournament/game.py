0ba48d94f4f01a97467bab60e9ecbef76a2e956a
Robledo-Villegas-Josue-Emiliano <RobledoTorpedo89@gmail.com>
2026-02-11 19:36:43 -0700

    Add some methods to "game.py"

--- Game_Tournament/game.py
+++ Game_Tournament/game.py
@@ -5,11 +5,12 @@ from Athlete import Athlete
 from Sport import Sport
 from Team import Team
 import random
+import json
 
 class Game: 
     """Gameclass represents a game in the tournament, It has two teams and a score"""
 
-    def __init__ (self, A:Team  , B:Team):
+    def __init__(self, A:Team  , B:Team):
         """Custom constructor for game class."""
         self.set_team(A, "local")
         self.set_team(B, "visitor")
@@ -26,7 +27,7 @@ class Game:
         else:
             raise ValueError ("Role must be 'local or 'visitor'.")
         
-    def play (self):
+    def play(self):
         """Simulate playing the game by randomly assigning  scores  ot each team. """
         self.score[self.team_a.name] = random.randint(0, Sport.max_score[self.team_a.sport.name])
         self.score[self.team_b.name] = random.randint(0, Sport.max_score[self.team_b.sport.name])
@@ -34,18 +35,39 @@ class Game:
     def __str__(self):
         """String representation of the Game class."""
         return f"{self.team_a.name} vs  {self.team_b.name} - Score: {self.score[self.team_a.name]} : {self.score[self.team_b.name]}"
-        
+    
+    def __repr__(self):
+        """String representation of the game class"""
+        return f"Game(team_a={repr(self.team_a)}, team_b={repr(self.team_b)}, score={self.score})"
+    
+    def to_json(self):
+        """String representation of the game class."""
+        """Convert the Game to a JSON string"""
+        return {
+            "team_a" : self.team_a.to_json(),
+            "team_b" : self.team_b.to_json(),
+            "Score"  : self.score
+        }
+
+    def a_game():
+        """Example usage of the game class."""
+        players_mex = ['Chicharito','Piojo','Guardado','Hector Moreno','Memo Ochoa','Gil Mora','Giovanni Dos Santos','Salcido','Hugo Sanches','Cuauthémoc Blanco','Jorge Campos']
+        players_brz = ['Ronaldinho','Neymar','Ronaldo Nazario','Alisson','Pelé','Marquinhos','Hulk','David Luis','Rapinha','Casemiro','Zico']
+        Sport = Sport("Futbol",11,"FIFA")
+        team_mx = Team("México",sport)
+        team_brz = Team("Brazil", sport)
+        for player in players_mex:
+            team_mx.add_athlete(Athlete(player))
+        for player in players_brz:
+            team_brz.add_athlete(Athlete(player))
+        game = Game(team_mx,team_brz)
+        game_string = game.to_json()
+        return game_string
+
+    def save_game_to_json(game, filename):
+        """Save the game object to a JSON file"""
+        with open(filename, 'w' , encodign='urf-8') as f:json.dump(game.to_json(), f, indent=4)
         
 if __name__ == "__main__":
-    a = Athlete("Chicharito")
-    b = Athlete("Memo Ochoa")    
-    s = Sport("Futbol",11,"FIFA")
-    mexico = Team("Mexico",s) 
-    mexico.add_athlete(a)
-    mexico.add_athlete(b)
-    brazil = Team("Brazil",s)
-    brazil.add_athlete(Athlete("Pelé"))
-    brazil.add_athlete(Athlete("Zico"))
-    game = Game(mexico,brazil)
-    game.play()
-    print(game)
+    string_game = a_game()
+    save_game_to_json(string_game, "game.json")
