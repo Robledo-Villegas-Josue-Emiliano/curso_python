d94ac2d0d587253bea249096f7bc89b318125422
Robledo-Villegas-Josue-Emiliano <RobledoTorpedo89@gmail.com>
2026-03-19 18:44:56 -0700

    Se agrega texto  y link entre index y about

--- hello_world/app.py
+++ hello_world/app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/') # Home or root of site
 def index():
-    return 'Hello World'
+    return '<html><head><title>HELLO WORLD</title></head><body><h1>Hello world</h1><p>Ir a <a href="/about">About</a></p></body></html>'
 
 @app.route('/about') 
 def about():
-    return 'About Page'
+    return '<html><head><title>HELLO WORLD</title>About this page</title></head><body> Everything about this website. Back to <a href="/">Hello world</a></body></html>'
 
 if __name__ == '__main__':
     app.run(debug=True)
